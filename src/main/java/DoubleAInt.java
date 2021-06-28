805613a8f0eac4618707e3393d6e225ddde1e3f9
Irene Alonso Rubio <irene.alonso@babel.es>
2021-06-28 12:08:04 +0200

    Programa Java erróneo

---
+++ src/main/java/DoubleAInt.java
@@ -0,0 +1,10 @@
+public class DoubleAInt {
+    public static void main (String[] args) {
+        int numero = getDouble();
+    }
+
+    private static double getDouble() {
+        int tres = 3
+        return tres;
+    }
+}
